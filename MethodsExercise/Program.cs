5806e07003cabb9e4b239951ab44932efded076f
Kassandra Johnson <kassandra.m.johnson@gmail.com>
2022-11-21 16:10:59 -0500

    Exercise 1 - ad lib story

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter your favorite holiday:");
+               string favHoliday = Console.ReadLine();
+            Console.WriteLine("Enter a type of building:");
+               string building = Console.ReadLine();
+            Console.WriteLine("Enter your favorite animal:");
+               string favAnimal = Console.ReadLine();
+            Console.WriteLine("Enter a type of container:");
+               string container = Console.ReadLine();
+            Console.WriteLine("Enter the name of your favorite celebrity:");
+               string favCelebrity = Console.ReadLine();
+            Console.WriteLine("");
+            Console.WriteLine($"Twas the night before {favHoliday}, \n" +
+                $"when all through the {building} not a creature was stirring, \n" +
+                $"not even a {favAnimal}.  \n" +
+                $"The {container}s were hung by the chimney with care \n" +
+                $"in the hopes that {favCelebrity} soon would be there.");
         }
     }
 }
